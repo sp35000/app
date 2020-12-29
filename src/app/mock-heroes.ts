e9c8ff9367752844d3406fa7a0ed81292e5f3573
Celso Kikuchi <celso.kikuchi@serpro.gov.br>
2020-12-29 15:19:30 -0300

    feat: until 06-Components

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Spider-Man' },
+  { id: 12, name: 'Captain America' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Thor' },
+  { id: 15, name: 'Iron Man' },
+  { id: 16, name: 'Luke Cage' },
+  { id: 17, name: 'Doctor Strange' },
+  { id: 18, name: 'Daredevil' },
+  { id: 19, name: 'Ant-Man' },
+  { id: 20, name: 'Wolverine' }
+];
